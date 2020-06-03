e3d9bd3045733affe96f2152000019a1478959a8
Javi Leyva <jvrleyva@gmail.com>
2020-06-03 17:11:37 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -12,10 +12,7 @@ puts remove_strawberry(contacts = {
 })
   
 def remove_strawberry(contacts)
-  # Assign Freddy Mercury's hash to a variable
   freddy = contacts["Freddy Mercury"]
-
-  # Iterate over only Freddy Mercury's data
   freddy.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
       value.delete_if{ |ice_cream| ice_cream == "strawberry" }
@@ -24,4 +21,4 @@ def remove_strawberry(contacts)
   contacts
 end 
   
-end
+
