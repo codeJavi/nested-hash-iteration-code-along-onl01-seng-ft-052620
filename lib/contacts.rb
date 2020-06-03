f7534613a3e69c4ffdcd366777839276b116a8aa
Javi Leyva <jvrleyva@gmail.com>
2020-06-03 17:16:03 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,9 +16,8 @@ def remove_strawberry(contacts)
   freddy.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
       value.delete_if{ |ice_cream| ice_cream == "strawberry" }
-    end
-  end
-  contacts
+end
+ 
 end 
   
 
