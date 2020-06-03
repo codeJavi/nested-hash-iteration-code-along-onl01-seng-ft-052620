dd6ba92c4ae868248223541368d2ccfad3cee822
Javi Leyva <jvrleyva@gmail.com>
2020-06-03 17:18:51 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -10,6 +10,8 @@ puts remove_strawberry(contacts = {
     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   }
 }
+
+end 
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
